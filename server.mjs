e1f653369b39bc61343244214a753f2f4f1f33cf
George Sofianos <georgesofianosgr@gmail.com>
2022-03-20 21:00:50 +0200

    initial commit

---
+++ server.mjs
@@ -0,0 +1,21 @@
+import express from 'express'
+const app = express()
+
+const loadAndRunModule = async (moduleName, data) => {
+  const plugin = await import('./modules/' + moduleName + '.mjs');
+  return plugin.default(data);
+}
+
+app.get('/module/:module', async function (req, res) {
+  console.log(req.params.module);
+  try {
+    const moduleName = req.params.module;
+    const response = await loadAndRunModule(moduleName)
+    res.send(response ? JSON.stringify(response) : 'No Data Returned From Module')
+  } catch(error) {
+    console.error(error);
+    res.status(500).send('Internal Error')
+  }
+})
+
+app.listen(3000)
